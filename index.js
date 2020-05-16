5abe71f51139edd813a70a86e38b1e06f43be824
randa yahya <randaalrayan96@gmial.com>
2020-05-16 00:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
-  let string =`Wake ${dogName} the ${dogBreed}`;
+  let string =`Wake ${dogName} the ${dogBreed}`
   return string;
 }
 function leashDog(dogName, dogBreed) {
   console.log(`Leash  ${dogName} the ${dogBreed}`);
-  let string =`Leash  ${dogName} the ${dogBreed}`;
+  let string =`Leash  ${dogName} the ${dogBreed}`
   return string;
 }
 function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
-  let string =`Walk to the park with ${dogName} the ${dogBreed}`;
+  let string =`Walk to the park with ${dogName} the ${dogBreed}`
   return string;
 }
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  let string =`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  let string =`Throw the frisbee for ${dogName} the ${dogBreed}`
   return string;
 }
 function walkHome(dogName, dogBreed) {
   console.log(`Walk home with Byron ${dogName} the ${dogBreed}`);
-  let string =`Walk home with Byron ${dogName} the ${dogBreed}`;
+  let string =`Walk home with Byron ${dogName} the ${dogBreed}`
   return string;
 }
 function unleashDog(dogName, dogBreed) {
  console.log(`Unleash ${dogName} the ${dogBreed}`);
-  let string =`Unleash ${dogName} the ${dogBreed}`;
+  let string =`Unleash ${dogName} the ${dogBreed}`
   return string;
 }
 
