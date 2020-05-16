3ad6cb7a4a1f0a197510f272e4760931af4ed469
randa yahya <randaalrayan96@gmial.com>
2020-05-16 00:27:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  let string =`Wake ${dogName} the ${dogBreed}`;
+  return string;
+}
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash  ${dogName} the ${dogBreed}`);
+  let string =`Leash  ${dogName} the ${dogBreed}`;
+  return string;
+}
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  let string =`Walk to the park with ${dogName} the ${dogBreed}`;
+  return string;
+}
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  let string =`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  return string;
+}
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk home with Byron ${dogName} the ${dogBreed}`);
+  let string =`Walk home with Byron ${dogName} the ${dogBreed}`;
+  return string;
+}
+function unleashDog(dogName, dogBreed) {
+ console.log(`Unleash ${dogName} the ${dogBreed}`);
+  let string =`Unleash ${dogName} the ${dogBreed}`;
+  return string;
+}
+
+function exerciseDog (dogName,dogBreed){
+ const routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
+ for(let i=0; i<routine.length; i++){
+   let value= routine[i](dogName,dogBreed));
+ }
+ return value
+}
